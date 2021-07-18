e7c1404ee823e6f1006748f25c7d4dd6c41c46f5
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2021-07-17 21:20:18 -0300

    create table component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-
 // import styled from './Home.module.css';
 
+import Table from '../components/Table';
+
 const Home: React.FC = () => {
-  return <div>hello</div>;
+  return (
+    <>
+      <h1>Logo</h1>
+
+      <Table />
+    </>
+  );
 };
 
 export default Home;
---
+++ src/pages/components/Table/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Table = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>title</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>data</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
